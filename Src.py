85b4d4c472cda9e2b2be2ed38e8b158c62d7e337
Abdelrahman26 <43171345+Abdelrahman26@users.noreply.github.com>
2020-12-01 20:33:52 +0200

    Create Pipeline

--- Src.py
+++ Src.py
@@ -5,6 +5,7 @@ import matplotlib.image as mpimg
 import numpy as np
 import cv2 
 import math
+import os
 %matplotlib inline
 
 # Helper Functions
@@ -45,4 +46,21 @@ def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
 
 def weighted_img(img, initial_img, α=0.8, β=1., γ=0.):
    return cv2.addWeighted(initial_img, α, img, β, γ)
-   
+# pipeline 
+# gray scale 
+img2 = cv2.imread("test_images/solidWhiteRight.jpg")
+h , w, c = img2.shape
+img_gray2 = grayscale(img2)
+# Filtring 
+img_filter2 = gaussian_blur(img_gray2, 9)
+# canny 
+img_edge2 = canny(img_filter2, 50, 200)
+#Only keeps the region of the image defined by the polygon 
+verticies = np.array([ [140,h] , [450,320] , [510, 320] , [w - 100, h] ] , np.uint)
+masked_img2= region_of_interest(img_edge2,verticies)
+# hough transform 
+lines2 = hough_lines(masked_img2, 1, np.pi/180, 10 , 1 ,50)
+#
+rst   = weighted_img(lines2, img2)
+
+
