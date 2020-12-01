05dfd2231e07d9818bd87f3781cb8da840fac183
Abdelrahman26 <43171345+Abdelrahman26@users.noreply.github.com>
2020-12-01 20:29:48 +0200

    Helper Functions 2

--- Src.py
+++ Src.py
@@ -29,3 +29,20 @@ def region_of_interest(img, vertices):
   masked_image = cv2.bitwise_and(img, mask)
   return masked_image
 
+def draw_lines(img, lines, color=[0, 0, 255], thickness=2):
+   cnt = 0
+   for line in lines:
+        cnt = cnt + 1
+        for x1,y1,x2,y2 in line:
+            cv2.line(img, (x1, y1), (x2, y2), color, thickness)
+            slope = (y1 - y2) / (x1 - x2)
+            
+def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
+   lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]), minLineLength=min_line_len, maxLineGap=max_line_gap)
+   line_img = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
+   draw_lines2(line_img, lines)
+   return line_img
+
+def weighted_img(img, initial_img, α=0.8, β=1., γ=0.):
+   return cv2.addWeighted(initial_img, α, img, β, γ)
+   
